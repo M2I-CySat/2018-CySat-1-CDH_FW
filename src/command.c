94526f77754c9fedfde7512c28fe18cb376a6e4d
Jake Drahos <drahos@iastate.edu>
2014-01-29 19:40:36 -0600

    Major rework of command.c
    No major functional changes, just made everything a whole lot
    prettier
    
    Also added some new commands and queries
    
    COMMAND BURN
    COMMAND POW_PRINT
    
    QUERY POW_PANEL
    
    Everything is untested! Yay!

--- src/command.c
+++ src/command.c
@@ -21,10 +21,11 @@
 #include <system.h>
 #include <string.h>
 #include <power.h>
+#include <helium.h>
 #include <nichrome.h>
 
 #define uartRX_BLOCK_TIME   ( ( portTickType ) 0xffff )
-#define MAX_COMMAND_LENGTH 100
+#define MAX_COMMAND_LENGTH 75
 #define MAX_QUERY_SUBTYPE_LENGTH 12
 #define MAX_TYPE_LENGTH 12
 
@@ -32,159 +33,226 @@
 #define UART1_ECHO 1
 #define UART2_ECHO 0
 
-/* Command Format
- *
- * For now, commands are formatted as follows:
- *
- * '!' is the start character, and '$' is the end character
- *
- *
- * Several cases can occur:
- *  - MAX_COMMAND_LENGTH exceeded - clear array, byteIndex = 0, store character
- *  - ! - clear array, byteIndex = 0, store character
- *  - $ - Store character, Handle the command
- *  - any other character - store and increment index
- *
- *  In this way, it will always be storing strings since the last start character. If
- *  command[0] is not ! (i.e. an overflow occurred some time or whatever) the command is invalid and
- *  will be rejected by HandleCommand
- *
- */
+#define CHECSUM_MESSAGES 0
+#define CONSOLE_MESSAGES 1
+#define RADIO_MESSAGES 0
 
-static void prvDoQueryTime() {
-        vConsolePrintf("!RESULT,TIME,It's been many seconds since 1970,C3$");
+
+/* FUNCTION PROTOTYPES */
+
+static char sendMessage(char);
+static void prvHandleQuery(char **, int);
+static void prvHandleCommand(char **, int);
+static void prvHandleMessage(char *);
+static void prvHandlePowerPanelQuery(char **, int);
+
+
+/* FUNCTION BODIES */
+
+static inline void nackType() {
+    sendMessage("NACK_ERROR,TYPE");
 }
 
-static void prvDoQueryMtime() {
-        vConsolePrintf("!RESULT,MTIME,Long time in space...,E4$");
+static inline void nackSubtype() {
+    sendMessage("NACK_ERROR,SUBTYPE");
 }
 
-static void prvDoQueryHello() {
-        vConsolePrintf("!RESULT,HELLO,Hello World,A1$");
+static inline void nackLength() {
+    sendMessage("NACK_ERROR,LENGTH");
 }
 
-static void prvConsoleHandleQuery(char * fields[], int fieldCount) {
-        if((strncmp("HELLO", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) && (fieldCount == 3))
-		prvDoQueryHello();
-        if((strncmp("TIME", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) && (fieldCount == 3))
-		prvDoQueryTime();
-        if((strncmp("MTIME", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) && (fieldCount == 3))
-		prvDoQueryMtime();
-        if((strncmp("POW_PRINT", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) && (fieldCount == 3)) {
+static void sendMessage(char * body) {
+    char * checksum = "a1";
+
+#if CHECKSUM_MESSAGES
+    //calculate an actual checksum
+#endif
+
+
+#if CONSOLE_MESSAGES
+    vConsolePrintf("!%s,%s$", body, checksum);
+#endif
+#if RADIO_MESSAGES
+    //TODO Sprintf the message, then heliumSendData
+    //vHeliumSendData("!%s,%s$", body, checksum);
+#endif
+
+}
+
+/*Message subtype handling*/
+void prvHandlePowerPanelQuery(char** fields, int fieldCount) {
+    powerData * housekeeping;
+    char resultBuffer[50];
+    memset(resultBuffer, 0, 50);
+    if (fields[2] == 'X') {
+        vPowerPollHousekeepingData();
+        housekeeping = xPowerGetHousekeepingData();
+        sprintf(resultBuffer, "RESULT,POW_PANEL,X,%4X,%4X,%4X", housekeeping->XVoltage, housekeeping->XCurrent0, housekeeping->XCurrent1);
+        sendMessage(resultBuffer);
+    }
+    else if (fields[2] == 'Y') {
+        vPowerPollHousekeepingData();
+        housekeeping = xPowerGetHousekeepingData();
+        sprintf(resultBuffer, "RESULT,POW_PANEL,Y,%4X,%4X,%4X", housekeeping->YVoltage, housekeeping->YCurrent0, housekeeping->YCurrent1);
+        sendMessage(resultBuffer);
+    }
+    else if (fields[2] == 'Z') {
+        vPowerPollHousekeepingData();
+        housekeeping = xPowerGetHousekeepingData();
+        sprintf(resultBuffer, "RESULT,POW_PANEL,Z,%4X,%4X,%4X", housekeeping->ZVoltage, housekeeping->ZCurrent0, housekeeping->ZCurrent1);
+        sendMessage(resultBuffer);
+    }
+    else
+        sendMessage("NACK_ERROR,PARAM,Invalid Axis");
+}
+
+/*Message Type Handling*/
+static void prvHandleQuery(char * fields[], int fieldCount) {
+    if(strncmp("HELLO", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) {
+        if (fieldCount == 3)
+            sendMessage("RESULT,HELLO,Hello World");
+        else
+            nackLength();
+    }
+    else if (strncmp("POW_PANEL", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) {
+        if (fieldCount == 4)
+            prvHandlePowerPanelQuery();
+        else
+            nackLength();
+    }
+    else
+        nackSubtype();
+}
+
+static void prvHandleCommand(char * fields[], int fieldCount) {
+    if(strncmp("BURN", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) {
+        if (fieldCount == 3)
+            vNichromeStartTask();
+        else
+            nackLength();
+    }
+    else if(strncmp("POW_PRINT", fields[1], MAX_QUERY_SUBTYPE_LENGTH) == 0) {
+        if (fieldCount == 3) {
             vPowerPollHousekeepingData();
             vPowerPrintHousekeepingData();
         }
-
+        else
+            nackLength();
+    }
+    else
+        nackSubtype();
 }
 
-static void prvHandleCommand(char * command) {
-	if (!command[0] == '!') return; //invalid command
+static void prvHandleMessage(char * command) {
+    if (command[0] != '!') return; //invalid command
 
-	//VALIDATE CHECKSUM HERE -- ANY LATER AND IT WILL GET TRASHED BY PARSING
+#if CHECKSUM_MESSAGES
+    //VALIDATE CHECKSUM HERE -- ANY LATER AND IT WILL GET TRASHED BY PARSING
+#endif
 
+    int fieldCount = 1;
+    int currentField = 0;
+    int len = strlen(command);
+    int i;
+    for(i = 0; i < len; i++) {
+        if (command[i] == ',') fieldCount++;
+    }
 
-	int fieldCount = 1;
-	int currentField = 0;
-	int len = strlen(command);
-	int i;
-	for(i = 0; i < len; i++) {
-		if (command[i] == ',') fieldCount++;
-	}
+    if (fieldCount < 3) return; //invalid command
 
-	if (fieldCount < 3) return; //invalid command
+    char * fields[fieldCount]; 	//pointer to beginning of each field
 
-	char * fields[fieldCount]; 	//pointer to beginning of each field
+    fields[currentField] = command + 1; //first field begins at command[1]
 
-	fields[currentField] = command + 1; //first field begins at command[1]
+    for(i = 0; i < len; i++) {
+        if (command[i] == ',') {
+            command[i] = 0; //set the , to an ascii null to let strcmp work
+            fields[++currentField] = ((command + i) + 1); //the next field begins at comma + 1
+        }
+    }
 
-	for(i = 0; i < len; i++) {
-		if (command[i] == ',') {
-			command[i] = 0; //set the , to an ascii null to let strcmp work
-			fields[++currentField] = ((command + i) + 1); //the next field begins at comma + 1
-		}
-	}
+    command[len - 1] = 0;  	// replace stop character with ascii null.
+    // The command buffer now holds just \0 separated fields.
+    //Each field is pointed to by fields[]
 
-	command[len - 1] = 0;  	// replace stop character with ascii null.
-		// The command buffer now holds just \0 separated fields.
-                //Each field is pointed to by fields[]
-
-	if ((strncmp("QUERY", fields[0], MAX_TYPE_LENGTH) == 0) && (fieldCount >= 3))
-		prvConsoleHandleQuery(fields, fieldCount);
-        //TODO: THIS MUST BE MADE TO FIT THE PROTOCOL DOCUMENT.
-	if ((strncmp("BURN", fields[0], MAX_TYPE_LENGTH) == 0) && (fieldCount >= 3))
-            vNichromeStartTask();
-
+    if (strncmp("QUERY", fields[0], MAX_TYPE_LENGTH) == 0)
+        prvHandleQuery(fields, fieldCount);
+    //TODO: THIS MUST BE MADE TO FIT THE PROTOCOL DOCUMENT.
+    else if (strncmp("COMMAND", fields[0], MAX_TYPE_LENGTH) == 0)
+        prvHandleQuery(fields, fieldCount);
+    else
+        nackType();
 }
 
-static char rxByte() {
-	signed char byte = 0;
+static inline char rxByte() {
+    signed char byte = 0;
 
-	while (!xSerialGetChar(xUart1Handle, &byte, uartRX_BLOCK_TIME));
-	return byte;
+    while (!xSerialGetChar(xUart1Handle, &byte, uartRX_BLOCK_TIME));
+    return byte;
 }
 
 
 static void vUart1RXTask( void * params) {
-	char commandBuffer[MAX_COMMAND_LENGTH];
-	memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
+    char commandBuffer[MAX_COMMAND_LENGTH];
+    memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
 
-	int byteIndex = 0;
-	char byte;
+    int byteIndex = 0;
+    char byte;
 
-	for (;;) {
-		byte = rxByte();
+    for (;;) {
+        byte = rxByte();
 
 #if UART1_ECHO
-                vConsolePut(byte);
+        vConsolePut(byte);
 #endif
 
-		if ((byteIndex == MAX_COMMAND_LENGTH) || (byte == '!')) {
-			byteIndex = 0;
-			memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
-		}
+        if ((byteIndex == MAX_COMMAND_LENGTH) || (byte == '!')) {
+            byteIndex = 0;
+            memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
+        }
 
-		commandBuffer[byteIndex] = byte;
-		byteIndex ++;
+        commandBuffer[byteIndex] = byte;
+        byteIndex ++;
 
-		if (byte == '$') {
-			prvHandleCommand(commandBuffer);
-		}
-	}
+        if (byte == '$') {
+            prvHandleMessage(commandBuffer);
+        }
+    }
 }
 static void vUart2RXTask( void * params) {
-	char commandBuffer[MAX_COMMAND_LENGTH];
-	memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
+    char commandBuffer[MAX_COMMAND_LENGTH];
+    memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
 
-	int byteIndex = 0;
-	char byte;
+    int byteIndex = 0;
+    char byte;
 
-	for (;;) {
-		byte = rxByte();
+    for (;;) {
+        byte = rxByte();
 
 #if UART2_ECHO
-                vConsolePut(byte);
+        vConsolePut(byte);
 #endif
 
-		if ((byteIndex == MAX_COMMAND_LENGTH) || (byte == '!')) {
-			byteIndex = 0;
-			memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
-		}
+        if ((byteIndex == MAX_COMMAND_LENGTH) || (byte == '!')) {
+            byteIndex = 0;
+            memset(commandBuffer, 0, MAX_COMMAND_LENGTH);
+        }
 
-		commandBuffer[byteIndex] = byte;
-		byteIndex ++;
+        commandBuffer[byteIndex] = byte;
+        byteIndex ++;
 
-		if (byte == '$') {
-			prvHandleCommand(commandBuffer);
-		}
-	}
+        if (byte == '$') {
+            prvHandleCommand(commandBuffer);
+        }
+    }
 }
 
 void xStartUart1CommandHandling() {
     vConsolePrint("Starting command handling on UART1\r\n");
-    xTaskCreate( vUart1RXTask, NULL, configMINIMAL_STACK_SIZE, NULL, systemPRIORITY_UART1, NULL );
+    xTaskCreate( vUart1RXTask, NULL, configMINIMAL_STACK_SIZE + 100, NULL, systemPRIORITY_UART1, NULL );
 }
 
 void xStartUart2CommandHandling() {
     vConsolePrint("Starting command handling on UART2\r\n");
-    xTaskCreate( vUart2RXTask, NULL, configMINIMAL_STACK_SIZE, NULL, systemPRIORITY_UART1, NULL );
+    xTaskCreate( vUart2RXTask, NULL, configMINIMAL_STACK_SIZE + 100, NULL, systemPRIORITY_UART2, NULL );
 }
