bc3590961163cce6de863463918789cfbd1849cf
Jake Drahos <drahos@iastate.edu>
2016-02-07 03:36:36 -0600

    Wrote printf header

---
+++ Inc/printf.h
@@ -0,0 +1,26 @@
+/* CySat printf() and friends
+ *
+ * This printf() implementation
+ * is RTOS-friendly and easy to 
+ * use for debug purposes.
+ */
+ 
+ #ifndef __PRINTF_H
+ #define __PRINTF_H
+ 
+ #include <stm32f4xx_hal_uart.h>
+ 
+ /* Call printf to a specific uart */
+ int uprintf(USART_TypeDef uart, const char *format_string, ...);
+ 
+ /* Wraps uprintf() and prints to the debug UART */
+ int dbg_printf(const char *format_string, ...);
+ 
+ 
+ /* One shared buffer is used for all printf's across all tasks.
+  * This buffer is only used for formatting; it is atomically passed to
+  * the UART drivers.
+  */
+ #define PRINTF_BUFFER_SIZE 128
+ 
+ #endif
