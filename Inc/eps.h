7522d73ed387b16db2eb4f3faa70f9e4c3f2aa51
Jake Drahos <j@kedrahos.com>
2018-01-24 16:05:24 -0600

    Wrote RTOS boilerplate for EPS

--- Inc/eps.h
+++ Inc/eps.h
@@ -4,13 +4,13 @@
 #define EPS_BLOCK 			0x01
 #define EPS_STORE_RESULT 	0x02
 
-struct eps_values {
+struct eps_telemetry {
 };
 
-/* Request the EPS thread to update its latest values */
-int EPS_UpdateValues(int flags);
+/* Request the EPS thread to update its latest telemetry */
+int EPS_UpdateTelemetry(int flags);
 
-/* Read the latest values from the EPS */
-int EPS_GetValues(struct eps_values * out);
+/* Read the latest telemetry from the EPS */
+int EPS_GetTelemetry(struct eps_values * out);
 
 #endif
--- Src/eps.c
+++ Src/eps.c
@@ -4,13 +4,16 @@
 #include "assert.h"
 
 struct eps_queue_item {
+	int flags;
 };
 
 osMailQId eps_queueHandle;
 osMailQDef(eps_queue, 4, struct eps_queue_item);
 
+static struct eps_telemetry telemetry;
+
 /* Update stored values */
-static int updateValues(void);
+static int updateTelemetry(void);
 
 void EPSTask()
 {
@@ -29,17 +32,62 @@ void EPSTask()
 	 *
 	 * */
 	for (;;) {
-		osDelay(10000);
+		osEvent evt;
+		evt = osMailGet(eps_queueHandle, osWaitForever);
+		if (evt.status == osEventMail) {
+			struct esp_queue_item * item = evt.value.p;
+
+			/* Do things with item based on item.flags */
+		} else {
+			Debug_Printf("Unexpected status for EPS queue")
+		}
 	}
 }
 
-/* Create an EPS queue item and push it to the queue */
-int EPS_UpdateValues(int flags)
+int EPS_UpdateTelemetry(int flags)
 {
+	/* Create an EPS queue item and push it to the queue.
+	 * 
+	 * If the blocking flag was specified, wait on the semaphore.
+	 */
+
+	struct eps_queue_item * item = osMailAlloc(eps_queueHandle, 0);
+	if (item == NULL) {
+		Debug_Printf("Error allocating item from EPS queue pool");
+		return -1;
+	}
+
+	item.flags = flags;
+
+	if (osMailPut(eps_queueHandle, item)) {
+		Debug_Printf("Error queueing item for send!");
+		osMailFree(eps_queueHandle, item);
+		return -1;
+	}
+
+	/* Maybe wait on semaphore, I don't care */
+
+	return 0;
+}
+
+int EPS_GetTelemetry(struct eps_telemetry * out)
+{
+	/* Lock the EPS mutex
+	 * Copy the telemetry struct to out
+	 * Release the EPS mutex
+	 */
+
 	return -1;
 }
 
-int EPS_GetValues(struct eps_values * out)
+static int updateTelemetry(void)
 {
+	/* Lock the SYS_I2C Mutex
+	 * Perform a transaction with the EPS
+	 * Release the I2C mutex
+	 * Lock the EPS mutex
+	 * Parse the telemtry and populate the struct 'telemetry'
+	 * Release the EPS mutex
+	 */
 	return -1;
 }
