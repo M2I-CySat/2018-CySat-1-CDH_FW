e6726743645b40e9e8b3bc84432a6030cf79f137
wrickert <brickert7@gmail.com>
2018-10-14 20:57:23 -0500

    finished tmux script

--- Docker/cysat-dev.sh
+++ Docker/cysat-dev.sh
@@ -1,4 +1,3 @@
-tmux new-session -d 'cysat-dev'
-tmux split-window -v 'openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg ; read'
-tmux split-window -v 'arm-none-eabi-gdb /app/firmware/build/cysat.elf ; read'
+tmux new-session 'openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg ; read' \; \
+	split-window 'arm-none-eabi-gdb /app/firmware/build/cysat.elf ; read'
 
