da10ac2a8b886cfa73ac3b2425db780d92b2ec54
William Rickert <brickert7@gmail.com>
2018-10-16 20:53:49 -0500

    script editing

--- Docker/cysat-dev.sh
+++ Docker/cysat-dev.sh
@@ -1,3 +1,3 @@
-tmux new-session 'openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg ; read' \; \
+tmux new-session -d 'openocd -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg ; read' \; \
 	split-window 'arm-none-eabi-gdb /app/firmware/build/cysat.elf ; read'
 
